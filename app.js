a109b4b8a63e2e247e8d45d5b04e707ac3f62467
Tomohito <>
2023-03-07 18:27:58 +0900

    クイズ1問目リファクタリング

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const question = "ゲーム市場、最も売れたゲーム機は次のうちどれ？";
 
-const answers = [
-  'A',
-  'B',
-  'C',
-  'D'
-];
+const answers = ["A", "B", "C", "D"];
 
-const correct = 'A';
-// テキスト要素の書き換え
+const correct = "A";
+
+const $button = document.getElementsByTagName("button");
+const btnlength = $button.length;
+
+
+// クイズの問題文の定義
+const setupQuiz = () =>{
 document.getElementById("js-question").textContent = question;
-document.getElementsByTagName("button")[0].textContent = answers[0];
-document.getElementsByTagName("button")[1].textContent = answers[1];
-document.getElementsByTagName("button")[2].textContent = answers[2];
-document.getElementsByTagName("button")[3].textContent = answers[3];
+let btnIndex =0;
+while(btnIndex < btnlength ){
+  $button[btnIndex].textContent = answers[btnIndex];
+  btnIndex++;
+}}
+setupQuiz();
 
+
+const clickHandler  =(e) => {
+  if (correct === e.target.textContent) {
+    window.alert("正解");
+  } else {
+    window.alert("不正解");
+  }
+};
 // クリックイベント(正誤判定)
-document.getElementsByTagName("button")[0].addEventListener( 'click', () =>{
-if(correct === document.getElementsByTagName("button")[0].textContent){
-window.alert('正解');
-} else {
-  window.alert('不正解');
+
+let handlerIndex = 0;
+while(handlerIndex < btnlength) {
+  $button[handlerIndex].addEventListener("click", (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
 }
-
-});
-
-// console.log(document.getElementById("js-question"));
