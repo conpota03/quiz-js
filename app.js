1897d3897d00ad9e3c6d13733cb31c8d55231627
conpota03 <119793548+conpota03@users.noreply.github.com>
2023-03-09 16:35:39 +0900

    Update app.js
    ランダム関数

--- app.js
+++ app.js
@@ -3,58 +3,83 @@ const quiz = [
     question: '次のうち桜はどれでしょうか',
     answers: [ 'ソメイヨシノ', 'ポインセチア', 'シクラメン'],
     correct: 'ソメイヨシノ'
-  }, {
+  },
+  {
     question: '数の子は何の卵でしょうか？',
     answers: [ 'イワシ', 'タイ', 'ニシン'],
     correct: 'ニシン'
-  }, {
+  },
+  {
     question: '春が旬の果物はどれでしょうか？',
     answers: [ 'スイカ', 'ユズ', '夏ミカン'],
     correct: '夏ミカン'
   }
 ];
-const quizLength = quiz.length;
+
+const $question = document.getElementById('js-question');
+const $buttons = document.querySelectorAll('.js-answer');
+const $result = document.getElementById('js-result');
+
+const shuffledQuiz = shuffle(quiz);
+const quizLength = shuffledQuiz.length;
 let quizIndex = 0;
 let score = 0;
 
-const $button = document.getElementsByTagName("button");
-const btnlength = $button.length;
-
-// クイズの問題文の定義
-const setupQuiz = () =>{
-document.getElementById("js-question").textContent = quiz[quizIndex].question;
-let btnIndex =0;
-while(btnIndex < btnlength ){
-  $button[btnIndex].textContent = quiz[quizIndex].answers[btnIndex];
-  btnIndex++;
-}}
-setupQuiz();
-
-const clickHandler  =(e) => {
-  if (quiz[quizIndex].correct === e.target.textContent) {
-    window.alert("正解");
-    score++;
-  } 
-  else {
-    window.alert("不正解");
+// Fisher-Yatesアルゴリズムによる配列のシャッフル
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-
-  quizIndex++;
-
-  if(quizIndex < quizLength) {
-  // 問題文があれば実行
-  setupQuiz();
-  }else{
-// 問題数がなければ実行
-window.alert('終了。あなたの正解数は' + score + '/' +quizLength + 'です！  ');
+  return arr;
 }
-};
-// クリックイベント正誤判定
 
-let handlerIndex = 0;
-while(handlerIndex < btnlength) {
-  $button[handlerIndex].addEventListener("click", (e) => {
-    clickHandler(e);
+// クイズの問題文と選択肢をセット
+function setQuiz() {
+  $question.textContent = shuffledQuiz[quizIndex].question;
+  $buttons.forEach((button, index) => {
+    button.textContent = shuffledQuiz[quizIndex].answers[index];
   });
-  handlerIndex++;
 }
+
+// 正解の場合の処理
+function handleCorrectAnswer() {
+  window.alert('正解！');
+  score++;
+}
+
+// 不正解の場合の処理
+function handleIncorrectAnswer() {
+  window.alert('不正解！');
+}
+
+// クイズの結果を表示
+function showResult() {
+  $result.textContent = `あなたの正解数は${score} / ${quizLength}です！`;
+}
+
+// クイズの初期化
+function initQuiz() {
+  setQuiz();
+  $result.textContent = '';
+}
+
+// ボタンがクリックされたときの処理
+$buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (shuffledQuiz[quizIndex].correct === button.textContent) {
+      handleCorrectAnswer();
+    } else {
+      handleIncorrectAnswer();
+    }
+    quizIndex++;
+    if (quizIndex < quizLength) {
+      setQuiz();
+    } else {
+      showResult();
+    }
+  });
+});
+
+// クイズの初期化
+initQuiz();
